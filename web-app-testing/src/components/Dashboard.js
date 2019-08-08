82dd59ae3ff2642285b9a0b285553ab036569354
masonkarsevar <masonkarsevar@gmail.com>
2019-08-08 15:53:17 -0700

    buttons tested in Dashboard

--- web-app-testing/src/components/Dashboard.js
+++ web-app-testing/src/components/Dashboard.js
@@ -4,10 +4,10 @@ function Dashboard(props) {
     return (
         <div className='dashboard-container'>
             <div className='button-container'>
-                <button className='strike-btn' onClick={() => props.handleStrike(props.strike, props.setStrike, props.setBall)}>Strike</button>
-                <button className='ball-btn' onClick={() => props.handleBall(props.ball, props.setBall, props.setStrike)}>Ball</button>
-                <button className='foul-btn' onClick={() => props.handleFoul(props.strike, props.setStrike, props.setBall)}>Foul</button>
-                <button className='hit-btn' onClick={() => props.handleHit(props.setBall, props.setStrike)}>Hit</button>
+                <button className='strike-btn' data-testid ='strike-btn' onClick={() => props.handleStrike(props.strike, props.setStrike, props.setBall)}>Strike</button>
+                <button className='ball-btn' data-testid ='ball-btn' onClick={() => props.handleBall(props.ball, props.setBall, props.setStrike)}>Ball</button>
+                <button className='foul-btn' data-testid ='foul-btn' onClick={() => props.handleFoul(props.strike, props.setStrike, props.setBall)}>Foul</button>
+                <button className='hit-btn' data-testid ='hit-btn' onClick={() => props.handleHit(props.setBall, props.setStrike)}>Hit</button>
             </div>
         </div>
     )
--- web-app-testing/src/components/Dashboard.test.js
+++ web-app-testing/src/components/Dashboard.test.js
@@ -2,11 +2,69 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import Dashboard from './Dashboard';
-import {render} from '@testing-library/react';
+import {render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 
 describe('Dashboard', () => {
   it('renders without crashing', () => {
     render(<Dashboard />)
   });
+
+  describe('ball button invoked', () => {
+    it('should run a function when clicked', () => {
+        const handleBall = (value=1, callback=(value) => value) => {
+            return callback(1 + value);
+        }
+        const mockFn = jest.fn();
+        const {getByTestId} = render(<Dashboard handleBall={mockFn} />);
+        const button = getByTestId('ball-btn');
+        fireEvent.click(button);
+
+        expect(mockFn).toHaveBeenCalled();
+    })
+    })
+
+    describe('strike button invoked', () => {
+        it('should run a function when clicked', () => {
+            const handleBall = (value=1, callback=(value) => value) => {
+                return callback(1 + value);
+            }
+            const mockFn = jest.fn();
+            const {getByTestId} = render(<Dashboard handleStrike={mockFn} />);
+            const button = getByTestId('strike-btn');
+            fireEvent.click(button);
+    
+            expect(mockFn).toHaveBeenCalled();
+        })
+        })
+
+        describe('hit button invoked', () => {
+            it('should run a function when clicked', () => {
+                const handleBall = (value=1, callback=(value) => value) => {
+                    return callback(1 + value);
+                }
+                const mockFn = jest.fn();
+                const {getByTestId} = render(<Dashboard handleHit={mockFn} />);
+                const button = getByTestId('hit-btn');
+                fireEvent.click(button);
+        
+                expect(mockFn).toHaveBeenCalled();
+            })
+        })
+
+            describe('foul button invoked', () => {
+                it('should run a function when clicked', () => {
+                    const handleBall = (value=1, callback=(value) => value) => {
+                        return callback(1 + value);
+                    }
+                    const mockFn = jest.fn();
+                    const {getByTestId} = render(<Dashboard handleFoul={mockFn} />);
+                    const button = getByTestId('foul-btn');
+                    fireEvent.click(button);
+            
+                    expect(mockFn).toHaveBeenCalled();
+                })
+            })
+
+  
 });
